c147e84c78964732ad6d3a6d3a401bde8541dd8d
rohansp004 <113821669+rohansp004@users.noreply.github.com>
2023-05-31 12:47:04 +0530

    Create IplDemo1.java

---
+++ IplDemo1.java
@@ -0,0 +1,16 @@
+class Practical{
+  public static void main(String[] rohan){
+    String str1 = new String("IPL 2023");
+    String str2 = new String("Winner: CSK" );
+    String str3 = new String("Runner up: GT");
+    String str4 = new String("Orange cap:  Gill");
+    String str5 = new String("Purple cap:  Shami ");
+    
+    System.out.println(str1);
+    System.out.println(str2);
+    System.out.println(str3);
+    System.out.println(str4);
+    System.out.println(str5);
+    
+  }
+}
